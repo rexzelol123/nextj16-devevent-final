57800ac335662bede79bb92732e87f359846af9c
Jon Rexzel Valloyas <valloyasjr@gmail.com>
2026-04-30 19:53:57 +0800

    deployment setup

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import {Suspense} from "react";
 import EventDetails from "@/components/EventDetails";
 
+
+
+
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }>}) => {
     const slug = params.then((p) => p.slug);
 
